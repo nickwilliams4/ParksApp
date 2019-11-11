69394fd4d830e8ce5cd16935d295d45ecc765885
nickwilliams4 <50678575+nickwilliams4@users.noreply.github.com>
2019-11-11 12:42:14 -0700

    update

--- app.js
+++ app.js
@@ -1,23 +1,46 @@
 'use strict';
-const search = $('.search').val()
-const searchURL = `https://developer.nps.gov/api/v1/parks?stateCode=${search}&api_key=xwZsd8iEC2eTtbMSILdCFUlOcveQmekwkkTbMYac`
 
-function getParks(search) {
-  fetch(searchURL)
-    .then(response => response.json())
-    .then(responseJson => 
-      displayResults(responseJson))
-    .catch(error => alert('Something went wrong. Try again later.'));
+
+function searchHandler() {
+  const search = $('.search').val();
+  if (search.trim() === '') {
+    $('.error-message').html('Please enter a state');
+  } else {
+    $('.search').val('');
+    $('.searchResults').empty();
+    $('.error-message').html('');
+    getParks(search);
+  }
 }
 
+function getParks(query) {
+  const searchURL = `https://developer.nps.gov/api/v1/parks?stateCode=${query}&api_key=xwZsd8iEC2eTtbMSILdCFUlOcveQmekwkkTbMYac`
+  fetch(searchURL)
+    .then(response => {
+      if (!response.ok) {
+        throw new Error(response.statusText);
+      }
+      return response.json();
+    })
+    .then(responseJson => 
+      displayResults(responseJson))
+    .catch(e => {
+      $('.error-message').html('Something went wrong. Try again later.');
+})
+}
+function displayResults(results) {
+  $('.searchResults').html(
+    results
+      .map(results => `<div>${data.states[0]}</div>`)
+      .join('')
+  );
+}
 
 function watchForm() {
   $('form').submit(event => {
     event.preventDefault();
-    getParks(search);
+    searchHandler();
   });
 }
 
 $(watchForm);
-
-console.log(search);
